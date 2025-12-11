8453f3a54674df11f6139ac2ab52d252b192d0b2
Lawson Hung <lawsonhung@yahoo.com>
2025-12-11 16:55:09 -0500

    Add try-catch statement

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // The provided course information.
+
+
 const CourseInfo = {
   id: 451,
   name: "Introduction to JavaScript"
@@ -75,27 +77,41 @@ const LearnerSubmissions = [
     }
   }
 ];
+try {
+  function getLearnerData(course, ag, submissions) {
 
-function getLearnerData(course, ag, submissions) {
-  // here, we would process this data to achieve the desired result.
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0 // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833 // late: (140 - 15) / 150
+
+    const courseID = course.id;
+    console.log(courseID);
+
+    // If an AssignmentGroup does not belong to its course (mismatching course_id), throw an error, letting the user know that the input was invalid.
+    if (courseID != ag.course_id) {
+      throw new Error(`You don't belong to this course`);
     }
-  ];
 
-  return result;
+
+    // // here, we would process this data to achieve the desired result.
+    // const result = [
+    //   {
+    //     id: 125,
+    //     avg: 0.985, // (47 + 150) / (50 + 150)
+    //     1: 0.94, // 47 / 50
+    //     2: 1.0 // 150 / 150
+    //   },
+    //   {
+    //     id: 132,
+    //     avg: 0.82, // (39 + 125) / (50 + 150)
+    //     1: 0.78, // 39 / 50
+    //     2: 0.833 // late: (140 - 15) / 150
+    //   }
+    // ];
+
+    // return result;
+  }
+
+} catch (err) {
+  console.error(err.message);
 }
-
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
 console.log(result);
